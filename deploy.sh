efffb28b95d84c4bb63d8f1faf2982f899032b73
Bao H.H <eh5@sokka.cn>
2019-12-16 17:36:09 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+DEFAULT_DEPLOY_REPO=git@github.com:v2fly/v2ray-step-by-step.git
+
+if [ -v DEPLOY_REPO ]; then
+  _deploy_repo=$DEPLOY_REPO
+else
+  _deploy_repo=$DEFAULT_DEPLOY_REPO
+fi
+
 echo -e "Start building..."
 vuepress build .
 
-echo -e "\n\nCheck if build prompted any errors.\nPress any key to countinue..."
-read
+if [ -v CI ]; then
+  echo -e "Running in CI environment"
+else
+  echo -e "\n\nCheck if build prompted any errors.\nPress any key to countinue..."
+  read
+fi
 
 cd .vuepress/dist
 echo "guide.v2fly.org" > CNAME
@@ -14,8 +26,7 @@ git config user.email SharedAccount+V2FlyContrib@unial.org
 git config commit.gpgsign false
 git add -A
 git commit -m "build $(date -u '+#%U%g%w-%N')"
-git push -f -v --progress git@github.com:v2fly/v2ray-step-by-step.git master:gh-pages
+git push -f -v --progress ${_deploy_repo} master:gh-pages
 cd ../
 rm -rfv dist/
 echo "Done."
-
